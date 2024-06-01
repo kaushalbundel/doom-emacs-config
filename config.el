c729ad25ee46f1a49777c45fde2e6cbaa28f226b
Kaushal Bundel <kaushalbundel@gmail.com>
2024-06-01 23:09:23 +0530

    avy related update
    avy can be used for better navigation.
    Changes done as per (https://karthinks.com/software/avy-can-do-anything/)

--- config.el
+++ config.el
@@ -239,8 +239,49 @@
 
 ;;avy-configuration
 ;;avy is a package that is used to easily navigate the buffer
-
 (map! "C-c g l" #'avy-goto-line
-      "C-c g w" #'avy-goto-word-0)
+      "C-c g w" #'avy-goto-word-0
+      "M-j" #'avy-goto-char-timer)
 
+;; function  to kill whole line or just a word
+;; usage is just select the k or K to kill the whole line and press the selection word
+(defun avy-action-kill-whole-line (pt)
+  (save-excursion
+    (goto-char pt)
+    (kill-whole-line))
+  (select-window
+   (cdr
+    (ring-ref avy-ring 0)))
+  t)
+
+(setf (alist-get ?k avy-dispatch-alist) 'avy-action-kill-stay
+      (alist-get ?K avy-dispatch-alist) 'avy-action-kill-whole-line)
+
+;; function for copying whole line
+(defun avy-action-copy-whole-line (pt)
+  (save-excursion
+    (goto-char pt)
+    (cl-destructuring-bind (start . end)
+        (bounds-of-thing-at-point 'line)
+      (copy-region-as-kill start end)))
+  (select-window
+   (cdr
+    (ring-ref avy-ring 0)))
+  t)
+
+(defun avy-action-yank-whole-line (pt)
+  (avy-action-copy-whole-line pt)
+  (save-excursion (yank))
+  t)
+
+(setf (alist-get ?y avy-dispatch-alist) 'avy-action-yank
+      (alist-get ?w avy-dispatch-alist) 'avy-action-copy
+      (alist-get ?W avy-dispatch-alist) 'avy-action-copy-whole-line
+      (alist-get ?Y avy-dispatch-alist) 'avy-action-yank-whole-line)
+
+;; function for zapping
+;; zapping is used to remove stuff between similar character. for eg, stuff between quotes or html tags
+
+
+;;consult
 (map! "C-c b" #'consult-buffer)
