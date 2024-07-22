fb2cd49ad19cc7b6096c85f4d2aa29c592058b3e
Kaushal Bundel <kaushalbundel@gmail.com>
2024-07-22 11:00:58 +0530

    Theme changed + org roam reverted
    The rationale is that denote solves my purpose completely. I do not need
    any additional tool which can improve my workflow.

--- config.el
+++ config.el
@@ -33,7 +33,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+(setq doom-theme 'modus-operandi-tinted)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -313,3 +313,5 @@
               ("C-c C-b" . obsidian-backlink-jump)
               ;; If you prefer you can use `obsidian-insert-link'
               ("C-c C-l" . obsidian-insert-wikilink)))
+
+;; setting org-roam
